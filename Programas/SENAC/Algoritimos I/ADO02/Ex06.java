d1f7c87825d6d3d493b632f3d07be023f468a750
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-04-17 19:36:23 -0300

    Add files via upload

---
+++ Programas/SENAC/Algoritimos I/ADO02/Ex06.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Ex06{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String palavra, palavraAux;
+        int i=0;
+
+        while(i == 0){
+            System.out.print("Escreva uma palavra que contenha F ou  começa com P: ");
+            palavra = ler.nextLine();
+            
+            palavra.toUpperCase();
+
+            if(palavra.substring(0).equals("P") ){
+                System.out.print("A sua palavra começa com P!: "+palavra);
+                i++;
+            }else if(palavra.indexOf("F")){
+                System.out.print("A sua palavra conte com F!: "+palavra);
+                i++;
+            }
+        }
+        
+    }
+}
