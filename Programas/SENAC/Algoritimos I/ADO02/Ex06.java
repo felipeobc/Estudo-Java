1f88f7166b432b68cd6977d8a92a8acc2c188652
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-04-18 18:10:15 -0300

    Update Ex06.java

--- Programas/SENAC/Algoritimos I/ADO02/Ex06.java
+++ Programas/SENAC/Algoritimos I/ADO02/Ex06.java
@@ -4,18 +4,19 @@ public class Ex06{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         String palavra, palavraAux;
-        int i=0;
+        int i=0, achou;
 
         while(i == 0){
             System.out.print("Escreva uma palavra que contenha F ou  começa com P: ");
             palavra = ler.nextLine();
             
-            palavra.toUpperCase();
-
+            palavra = palavra.toUpperCase();
+            achou = palavra.indexOf("F");
+            
             if(palavra.substring(0).equals("P") ){
                 System.out.print("A sua palavra começa com P!: "+palavra);
                 i++;
-            }else if(palavra.indexOf("F") != 0){
+            }else if(achou != 0){
                 System.out.print("A sua palavra conte com F!: "+palavra);
                 i++;
             }
