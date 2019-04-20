0c14676058331e8ad6369cf11f961fdf245607f2
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-04-19 21:11:59 -0300

    add  mais conteudo

---
+++ Programas/URI Online/Ex1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex1001{
+    public static void main(String[] args) {
+        int A, B, X;
+
+        Scanner(A);
+        Scanner(B);
+        
+
+        X = A + B;
+
+        System.out.println("X = "+ X);
+
+    }
+}
