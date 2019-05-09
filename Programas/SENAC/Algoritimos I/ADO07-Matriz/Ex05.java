043ffd0d7862abadec778cecbb30313f7a61cbb6
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-05-09 15:07:13 -0300

    atualizando Ex05

--- Programas/SENAC/Algoritimos I/ADO07-Matriz/Ex05.java
+++ Programas/SENAC/Algoritimos I/ADO07-Matriz/Ex05.java
@@ -21,12 +21,57 @@ public class Ex05{
            System.out.println("Escolha para andar: (1) - cima (2) - baixo (3) - esquerda (4) - direita (5) SAIR");
             escolha = ler.nextInt();
 
-            
 
-            if(escolha == 1){
 
+            if(escolha == 1){// cima
+                for(i = 0; i < matriz.length; i++){
+                    for(j = 0; j < matriz[j].length; j++){
+                        if(matriz[i][j] == "O"){
+                           matriz[i][j] = null;
+                           matriz[i-1][j-1] = "O"; 
+                        }
+                    }
+                }
             }
-       }while(sair == 0);
+            
+            if(escolha == 2){// baixo
+                for(i = 0; i < matriz.length; i++){
+                    for(j = 0; j < matriz[j].length; j++){
+                        if(matriz[i][j] == "O"){
+                           matriz[i][j] = null;
+                           matriz[i+1][j+1] = "O"; 
+                        }
+                    }
+                }
+            }
+            
+            if(escolha == 3){// esquerda
+                for(i = 0; i < matriz.length; i++){
+                    for(j = 0; j < matriz[j].length; j++){
+                        if(matriz[i][j] == "O"){
+                           matriz[i][j] = null;
+                           matriz[i][j-1] = "O"; 
+                        }
+                    }
+                }
+            }
+
+            if(escolha == 4){// direita
+                for(i = 0; i < matriz.length; i++){
+                    for(j = 0; j < matriz[j].length; j++){
+                        if(matriz[i][j] == "O"){
+                           matriz[i][j] = null;
+                           matriz[i][j+1] = "O"; 
+                        }
+                    }
+                }
+            }
+            
+            if(escolha == 5){
+                sair = 1;
+            }
+
+       }while(sair != 0);
         
     }
 }
