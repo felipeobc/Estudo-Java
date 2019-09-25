62aef1f8d78bd83a06e1ae7c0f487d1eb4dfd79d
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-09-24 21:15:32 -0300

    Create ADO01.java
    ado facil facil

---
+++ Programas/SENAC/Algoritimos II/ADO1/ADO01.java
@@ -0,0 +1,50 @@
+import static java.lang.System.out;
+
+import java.util.*;
+
+public class teste {
+
+    public static void main(String arg[]) {
+        int n, resposta, x;
+        n = 10000;
+        
+        int v[] = new int[n];
+        System.out.println("v[ ");
+        
+        // Scanner x = new Scanner(System.in);
+        Random r = new Random();
+      
+            
+        for (int i = 0; i < 10000; i++) {
+            
+            v[i] = r.nextInt(10000);
+        }
+        x = r.nextInt(10000);
+        
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + " ,");
+        }
+        out.println(" ]");
+
+        resposta = linear_search(x, v, n);
+
+        if (resposta != -1) {
+            System.out.println("O valor foi  encontrado: " + resposta);
+        } else {
+            System.out.println("O valor não encontrado");
+
+        }
+
+    }
+
+    
+    public static int linear_search(int a, int b[], int c){
+        int i;
+        for (i = 0; i < c; i++) {
+            if (b[i] == a) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
