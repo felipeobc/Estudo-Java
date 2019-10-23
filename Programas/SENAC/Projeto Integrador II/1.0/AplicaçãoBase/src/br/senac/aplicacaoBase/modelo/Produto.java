8d6557e5fc7d1d9c436f8b962d011f6febde8a24
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-10-23 17:09:18 -0300

    Atualização
    Update

---
+++ Programas/SENAC/Projeto Integrador II/1.0/AplicaçãoBase/src/br/senac/aplicacaoBase/modelo/Produto.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.aplicacaoBase.modelo;
+
+
+/**
+ *
+ * @author felipe.o.camargo
+ */
+public class Produto {
+    
+     //Atributos
+    private Integer id;
+    private String codigo;
+    private String nome;
+    private String especie;
+    private String valor;
+    private String estoque;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    public String getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(String estoque) {
+        this.estoque = estoque;
+    }
+}
+
+
